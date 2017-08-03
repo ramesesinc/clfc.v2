0f7b41867d0067a4da1a978b6d7b0062d42a46c5
Carl Louie Pernito <carlpernito@gmail.com>
2017-08-03 14:21:58 +0800

    updated the rameses-clfc-loan-services.jar

--- projects/server/rameses-clfc-loan-services.jar/loan/actions/CalculateLoanInterest.groovy
+++ projects/server/rameses-clfc-loan-services.jar/loan/actions/CalculateLoanInterest.groovy
@@ -8,7 +8,6 @@ public class CalculateLoanInterest implements RuleActionHandler {
 	def NS, LS;
 	public void execute(def params, def drools) {
 		def APP = params.app;
-		//println 'amount ' + params.amount.decimalValue;
 		APP.interest = LS.roundOffAmount(NS.round(params.amount.decimalValue));
 	}
 
